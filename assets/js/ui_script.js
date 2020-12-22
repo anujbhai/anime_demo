49d47a239b60d09643934df066248fa7e9d1e28f
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2020-12-22 19:15:00 +0530

    anuj/anime_demo_02: fixed timer and animation.

--- assets/js/ui_script.js
+++ assets/js/ui_script.js
@@ -1,60 +1,84 @@
 (function() {
+	console.log("Script loaded .. .. ..");
 	const doc = document;
 
 	doc.addEventListener("DOMContentLoaded", () => {
-		console.log("Test");
-		const timerSec = doc.querySelector(".display_second");
-		const timerMin = doc.querySelector(".display_minute");
-		let timeLeft = 59;
+		let counter = 0;
+		let timeleft = 60;
+		let interval;
 
-		const startBtn = doc.querySelector(".btn_play");
+		const playBtn = doc.querySelector(".btn_play");
 		const restartBtn = doc.querySelector(".btn_restart");
 		const pauseBtn = doc.querySelector(".btn_pause");
+		const timer = doc.querySelector("#timer_display");
 
 		let timerCtrlItemTxt = doc.querySelector("p.timer_control_item_text");
 
-		// Countdown timer
-		const countdownTimer = () => {
-			console.log("@TODO start timer and toggle play/pause");
+		/* ----- Format Time ----- */
+		const convertSeconds = (s) => {
+			let min = Math.floor(s / 60);
+			let sec = s % 60;
 
-			setInterval(function() {
-				if (timeLeft <= 0) {
-					clearInterval(timeLeft = 0);
-					sequence.pause();
-				}
-				timerMin.innerHTML = "0";
-				timerSec.innerHTML = timeLeft;
-				timeLeft -= 1;
-			}, 1000);
-		}; 
+			// Min & sec Eg.'00:59'
+			return `${min.toString().padStart(2, 0)} : ${sec.toString().padStart(2, 0)}`;
+		};
 
-		// Breathe-in/Breathe-out animation using AnimeJS
+		// Assign values to timer display
+		timer.innerHTML = convertSeconds(timeleft - counter);
+
+		/* ----- Activate timer ----- */
+		const timerRun = () => {
+			counter++;
+			timer.innerHTML = convertSeconds(timeleft - counter);
+
+			if (counter == timeleft) {
+				clearInterval(interval);
+				counter = 0;
+			}
+		};
+
+		const timerStart = () => {
+			interval = setInterval(timerRun, 1000);
+		}
+
+		const timerPause = () => {
+			clearInterval(interval);
+		};
+
+		/* ----- Breathe-in/Breathe-out animation using AnimeJS ----- */
 		// AnimeJS timeline and properties
 		const sequence = anime.timeline({
 			easing: "easeInOutQuad",
 			duration: 5000,
 			delay: 1000,
 			endDelay: 1000,
-			loop: true,
+			loop: false,
 			autoplay: false,
-			direction: "alternate",
+			direction: "normal",
 		});
 		// AnimeJS selector and values
 		sequence.add({
 			targets: ".circle_inner",
 			scale: 0.01,
 			backgroundColor: ["rgba(255,255,255, 1)", "rgba(255,255,255, 0.2)"],
-		}, 0);
+		})
+		.add({
+			targets: ".circle_inner",
+			scale: "100%",
+			backgroundColor: ["rgba(255,255,255, 0.2)", "rgba(255,255,255, 1)"],
+		}, "+=2000");
 
+		/* ----- Click events ----- */
 		// Click play button to start timer
-		startBtn.addEventListener("click", () => {
+		playBtn.addEventListener("click", () => {
 			// countdowntimer start
-			countdownTimer();
+			timerStart();
 
 			sequence.play();
 
-			if (startBtn.className == "btn btn_play active") {
-				startBtn.className = "btn btn_play ";
+			// Toggle play button state
+			if (playBtn.className == "btn btn_play active") {
+				playBtn.className = "btn btn_play ";
 				pauseBtn.className = "btn btn_pause active";
 				timerCtrlItemTxt.innerText = "Pause"
 			}
@@ -63,12 +87,14 @@
 		// Click pause button to hold animation
 		pauseBtn.addEventListener("click", () => {
 			// countdowntimer pause
+			timerPause();
 
 			sequence.pause();
 
+			// Toggle pause button state
 			if (pauseBtn.className == "btn btn_pause active") {
 				pauseBtn.className = "btn btn_pause ";
-				startBtn.className = "btn btn_play active";
+				playBtn.className = "btn btn_play active";
 				timerCtrlItemTxt.innerText = "Play"
 			}
 		});
@@ -79,3 +105,6 @@
 		});
 	});
 }) ();
+
+
+
